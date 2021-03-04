2b799826ac4fe75e193cd720755169503ef5c151
RajathaDH <59072510+RajathaDH@users.noreply.github.com>
2021-03-04 09:53:05 +0530

    Added basic route handling

--- NodeJS/vanilla/server.js
+++ NodeJS/vanilla/server.js
@@ -1,8 +1,24 @@
 const http = require('http');
 
+const breeds = require('../breeds.json');
+
 const server = http.createServer((req, res) => {
-    
+    if (req.url == '/api/breeds' && req.method == 'GET') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(breeds));
+    } else {
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: 'Not found' }));
+    }
 });
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+//res.statusCode = 200;
+//res.setHeader('Content-Type', 'text/html');
+//res.write('<h1>Hello</h1>');
+//res.end();
+
+//res.writeHead(200, { 'Content-Type': 'application/json' });
+//res.end(JSON.stringify(breeds));
