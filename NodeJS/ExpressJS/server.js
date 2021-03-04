07425fa6ed42f8f97d9b29aa416b3bc713413fd9
RajathaDH <59072510+RajathaDH@users.noreply.github.com>
2021-03-04 15:40:52 +0530

    Added route not found 404 handler

--- NodeJS/ExpressJS/server.js
+++ NodeJS/ExpressJS/server.js
@@ -12,5 +12,9 @@ const breedsRouter = require('./routes/breeds');
 
 app.use('/api/breeds', breedsRouter);
 
+app.use((req, res) => {
+    res.status(404).json({ message: 'Route not found' });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
