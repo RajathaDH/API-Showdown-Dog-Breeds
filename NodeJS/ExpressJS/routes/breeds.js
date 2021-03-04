33dfcedf6f30838939f4c089fac4902fdce9b198
RajathaDH <59072510+RajathaDH@users.noreply.github.com>
2021-03-04 15:19:37 +0530

    Added get single breed API route

--- NodeJS/ExpressJS/routes/breeds.js
+++ NodeJS/ExpressJS/routes/breeds.js
@@ -11,4 +11,17 @@ router.get('/', async (req, res) => {
     res.status(200).json(breeds);
 });
 
+// get a single breed by id
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    
+    const breed = await Breed.findById(id);
+
+    if (breed) {
+        res.status(200).json(breed);
+    } else {
+        res.status(404).json({ message: `Breed with id ${id} not found.` });
+    }
+});
+
 module.exports = router;
